18d6a1096068199dfc116baa5ced6318840d9ca6
TkachevNikita <ntkachev03@mail.ru>
2025-10-06 19:08:27 +0500

    base layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  protected readonly title = signal('rmk');
-}
+export class AppComponent {}
