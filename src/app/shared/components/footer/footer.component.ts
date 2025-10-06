436e4cb440b4851a19d3c766eaafa97ae1ba2255
TkachevNikita <ntkachev03@mail.ru>
2025-10-07 03:48:50 +0500

    categories

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TuiIcon],
 })
 export class FooterComponent {}
