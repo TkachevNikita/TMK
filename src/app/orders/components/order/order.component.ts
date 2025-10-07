647630055dae33e7a4bf8a123c94e135cb398d2f
alexa <annamov@skbkontur.ru>
2025-10-08 04:10:15 +0500

    order styles

--- src/app/orders/components/order/order.component.ts
+++ src/app/orders/components/order/order.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputComponent } from '../../../shared/components/input/input.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [InputComponent, ButtonComponent]
 })
 export class OrderComponent {}
