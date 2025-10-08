24f45e0b1ed07a0ef6a9f246e8b954413443191e
TkachevNikita <ntkachev03@mail.ru>
2025-10-08 14:54:53 +0500

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,12 @@ import { TuiRoot } from '@taiga-ui/core';
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 import { RouterOutlet } from '@angular/router';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [MainLayoutComponent, TuiRoot, RouterOutlet, JsonPipe],
+  imports: [MainLayoutComponent, TuiRoot, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  protected readonly window = window;
-}
+export class AppComponent {}
